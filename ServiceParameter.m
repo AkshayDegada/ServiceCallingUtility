d523b9230de3df898ffaf09943acec3377ab1356
Minesh Purohit <minesh.purohit1@googlemail.com>
2014-08-04 12:31:29 +0530

    Update ServiceParameter.m

--- ServiceParameter.m
+++ ServiceParameter.m
@@ -3,7 +3,7 @@
 //  test
 //
 //  Created by Minesh Purohit on 31/07/14.
-//  Copyright (c) 2014 Triforce Inc. All rights reserved.
+//  Copyright (c) 2014 MinuMaster. All rights reserved.
 //
 
 #import "ServiceParameter.h"
